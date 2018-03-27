344cac7276331ffdd6f229de25436b47b6e4bae7
Zohair Ahmed <zohair.ahmed@charter.com>
2018-03-26 20:14:45 -0600

    Added parser example to get count of dropdown items from walmart quantity

--- modules/inventory/handlers/v1/{merchant}/{productID}.js
+++ modules/inventory/handlers/v1/{merchant}/{productID}.js
@@ -1,5 +1,8 @@
 'use strict';
 const debug = require('debug')('mocks');
+const cheerio = require('cheerio');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 module.exports = {
     get: function (req, reply, next) {
@@ -7,12 +10,56 @@ module.exports = {
         let merchant = req.params.merchant;
         let productID = req.params.productID;
         var response = {
-            ConfirmHumanity: false,
-            CaptchaPublicKey: '6LfiVREUAAAAABl1xMAKQrPCasScDGKqjQ5dZ5x1',
             ProductID: productID,
-            Merchant: merchant
+            Merchant: merchant,
+            minimumInventory: 0
         };
 
-        reply(response).code(200);
+        if (merchant === "walmart") {
+
+        }
+
+        debug(`About to load the url`);
+        JSDOM.fromURL("https://www.walmart.com/ip/LEGO-Star-Wars-TM-BB-8-75187/163329791", {
+
+            referrer: "https://google.com/",
+
+            userAgent: "Mellblomenator/9000",
+            includeNodeLocations: true
+        }).then(dom => {
+            debug(`Time to cerealize`);
+            var document = dom.serialize();
+
+            debug(`Serialization is done. About to load the doc`);
+
+            var ch = cheerio.load(document, { decodeEntities: true });
+
+            let totalNumberOfItems = 0;
+
+            ch(".visuallyhidden option").each(function (i, elem) {
+                if (!isNaN(ch(elem).val())) {
+                    debug(`Item  ${i}: ${ch(elem).val()}`);
+                    totalNumberOfItems += 1;
+                }
+            });
+
+            debug(`Total number of items: ${totalNumberOfItems}`);
+
+            response.minimumInventory = totalNumberOfItems;
+
+            reply(response).code(200);
+
+            //debug(`Quantity: ${ch('#read-more')}`);
+
+
+            //debug(`URL: ${options.uri}`);
+            //debug(`Bullet Points: ${ch('#fbExpandableSectionContent').children().html().trimLeft().trimRight()}`);
+            // ch('#fbExpandableSectionContent').find('.a-list-item').each(function (i, elem) {
+
+            //     debug(`Bullet  ${i}: ${ch(elem).html().trim()}`);
+            // });
+            // debug(`Description: ${ch('#productDescription').html().trim()}`);
+            // debug(`SalesRank: ${ch('#SalesRank').html().trim()}`);
+        });
     }
 };
